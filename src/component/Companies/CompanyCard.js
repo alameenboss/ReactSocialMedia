96bc2138d58942815585d8f931ed897df2509b71
alameenboss <4115051+alameenboss@users.noreply.github.com>
2020-08-22 11:41:43 +0530

    Fixing the follow  message and view profile dynamically

--- src/component/Companies/CompanyCard.js
+++ src/component/Companies/CompanyCard.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router-dom'
 export default function CompanyCard(props) {
+    const follow = useRef(null);
+
+    const handleClick = (action, id) => {
+        if (action === 'Follow') {
+            if(follow.current.innerHTML === 'Following')
+                follow.current.innerHTML = 'Follow'
+            else
+            follow.current.innerHTML = 'Following'
+            
+        }
+    }
+
     return (
         <div className="col-lg-3 col-md-4 col-sm-6">
             <div className="company_profile_info">
@@ -9,15 +21,19 @@ export default function CompanyCard(props) {
                     <h3>{props.company.name}</h3>
                     <h4>{props.company.establish}</h4>
                     <ul>
-                        <li><Link to="#" className="follow">Follow</Link></li>
                         <li>
-                            <Link to="#" className="message-us">
+                            <Link to="#" ref={follow} onClick={() => handleClick('Follow', props.company.id)} className="follow">
+                                Follow
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={'/Message?company-id=' + props.company.id} className="message-us">
                                 <i className="fa fa-envelope"></i>
                             </Link>
                         </li>
                     </ul>
                 </div>
-                <Link to="company-profile" className="view-more-pro">View Profile</Link>
+                <Link to={'/companyprofile?company-id=' + props.company.id} className="view-more-pro">View Profile</Link>
             </div>
         </div>
 
--- src/component/Profiles/UserCard.js
+++ src/component/Profiles/UserCard.js
@@ -1,39 +1,58 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router-dom';
 
 function UserCard(props) {
-    return (
-        <div className="col-lg-3 col-md-4 col-sm-6 col-12">
-        <div className="company_profile_info">
-          <div className="company-up-info">
-            <img src={props.value.img} alt="" />
-    <h3>{props.value.name}</h3>
-    <h4>{props.value.work}</h4>
-            <ul>
-              <li>
-                <Link to="#" className="follow">
-                  Follow
+  const follow = useRef(null);
+  const hire = useRef(null);
+
+  const handleClick = (action, id) => {
+    //alert(action + ' user with id: ' + id)
+    if (action === 'Follow') {
+      if (follow.current.innerHTML === 'Following')
+        follow.current.innerHTML = 'Follow'
+      else
+        follow.current.innerHTML = 'Following'
+    }
+    if (action === 'Hire') {
+      if (hire.current.innerHTML === 'Hired')
+        hire.current.innerHTML = 'Hire'
+      else
+        hire.current.innerHTML = 'Hired'
+    }
+  }
+
+  return (
+    <div className="col-lg-3 col-md-4 col-sm-6 col-12">
+      <div className="company_profile_info">
+        <div className="company-up-info">
+          <img src={props.value.img} alt="" />
+          <h3>{props.value.name}</h3>
+          <h4>{props.value.work}</h4>
+          <ul>
+            <li>
+              <Link to='#' ref={follow} onClick={() => handleClick('Follow', props.value.id)} className="follow">
+                Follow
                 </Link>
-              </li>
-              <li>
-                <Link to="#" className="message-us">
-                  <i className="fa fa-envelope"></i>
+            </li>
+            <li>
+              <Link to={'/Message?user-id=' + props.value.id} className="message-us">
+                <i className="fa fa-envelope"></i>
+              </Link>
+            </li>
+            <li>
+              <Link to='#' ref={hire} onClick={() => handleClick('Hire', props.value.id)} className="hire-us">
+                Hire
                 </Link>
-              </li>
-              <li>
-                <Link to="#" className="hire-us">
-                  Hire
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <Link to="user-profile.html" className="view-more-pro">
-            View Profile
-          </Link>
+            </li>
+          </ul>
         </div>
+        <Link to={'/UserProfile?user-id=' + props.value.id} className="view-more-pro">
+          View Profile
+          </Link>
       </div>
-  
-    )
+    </div>
+
+  )
 }
 
 export default UserCard
